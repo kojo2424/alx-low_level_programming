5a158c4274e1c8edcac1d14e5df043401d6a1489
kojo2424 <asmahcoffie2424@gmail.com>
2023-10-11 02:02:14 -0700

    lowercase loop

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_alphanbet - this is a function
+ * Return: 0
+ */
+void print_alphabet(void);
+{
+	char lett;
+
+	for (lett = 'a'; lett <= 'z' lett++)
+		_putchar(lett);
+	putchar('\n');
+	return (0);
+}
