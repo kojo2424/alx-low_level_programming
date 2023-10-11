6f90ff39fc875a83f81d26ba92bebee28baa141d
kojo2424 <asmahcoffie2424@gmail.com>
2023-10-11 14:09:37 -0700

    another one

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,14 +5,17 @@
  */
 void print_alphabet_x10(void)
 {
-	char c;
-	int i = 0;
+	char ch;
+	int i;
 
-	while (i <= 9)
+	while (i < 10)
 	{
-		for (c = 'a'; c <= 'z'; c++)
-			_putchar(c);
-	}
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+		}
+
 	_putchar('\n');
 	i++;
+	}
 }
