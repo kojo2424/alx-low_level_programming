02c5d0edae81346b4ad1d472fd52d2b6eb873119
kojo2424 <asmahcoffie2424@gmail.com>
2023-10-13 16:39:51 -0700

    Multiplication of numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * mul - multiply 2 int number
+ * @a: first int number
+ * @b: second int number
+ *
+ * Return: addition of the multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
