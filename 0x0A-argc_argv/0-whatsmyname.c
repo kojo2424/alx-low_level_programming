464d98b92100296cdbb4d4f16b974589c7c8d8e4
kojo2424 <asmahcoffie2424@gmail.com>
2023-10-28 19:03:46 -0700

    my new name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - Entry point
+ * @argc: argument count
+ * @argv: variable 2
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	
+	printf("%s\n", argv[0]);
+	return (0);
+}
